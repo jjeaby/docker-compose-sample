788a408bb30be18aa6e3ed4c2339675ef4fba93c
jjeaby <jjeaby@gmail.com>
2021-01-05 15:17:06 +0900

    remove comment

--- flask1.py
+++ flask1.py
@@ -8,15 +8,10 @@ FLASK2_HOST = os.getenv("FLASK2_HOST")
 if FLASK2_HOST == None:
     FLASK2_HOST = "http://0.0.0.0:5002"
 
-
 @app.route('/<name>')
 def hello_name(name):
-    print("------------------------")
-    print(FLASK2_HOST + f"/{name}")
-    print("------------------------")
     response = requests.get(FLASK2_HOST + f"/{name}")
     return response.text
 
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0",port=5001)
