c5a9d6348ab42334997077fc563b96544e1f580f
jjeaby <jjeaby@gmail.com>
2021-01-05 15:02:26 +0900

    docker-compose container 간 network 통신 샘플

--- flask2.py
+++ flask2.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5002)
+    app.run(host="0.0.0.0", port=5002)
